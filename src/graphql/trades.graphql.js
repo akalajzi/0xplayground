2aa163f11a23067ae3ce85b024fb3b9894be3930
akalajzi <akalajzi@gmail.com>
2017-11-07 12:51:03 +0100

    Obsolete Trades table queries

--- src/graphql/trades.graphql.js
+++ src/graphql/trades.graphql.js
@@ -1,35 +1,40 @@
-import gql from 'graphql-tag'
+/***
+  OBSOLETE - SHOULD NOT BE USED
+***/
 
-const TRADES_FRAGMENT = gql`
-  fragment TradesFragment on Trades {
-    id
-    address
-    timestamp
-    blockNumber
-    transactionHash
-    transactionIndex
-    gasUsed
-    gasPrice
-    blockHash
-    event
-    removed
-    filledMakerTokenAmountNormalized
-    filledTakerTokenAmountNormalized
-    price
-    invertedPrice
-    args
-  }
-`
 
-const TRADES_LIST = gql`
-  query Trades {
-    allTradeses (first: 100, orderBy: timestamp_DESC) {
-      ...TradesFragment
-    }
-  }
-  ${TRADES_FRAGMENT}
-`
-
-export {
-  TRADES_LIST,
-}
+// import gql from 'graphql-tag'
+//
+// const TRADES_FRAGMENT = gql`
+//   fragment TradesFragment on Trades {
+//     id
+//     address
+//     timestamp
+//     blockNumber
+//     transactionHash
+//     transactionIndex
+//     gasUsed
+//     gasPrice
+//     blockHash
+//     event
+//     removed
+//     filledMakerTokenAmountNormalized
+//     filledTakerTokenAmountNormalized
+//     price
+//     invertedPrice
+//     args
+//   }
+// `
+//
+// const TRADES_LIST = gql`
+//   query Trades {
+//     allTradeses (first: 100, orderBy: timestamp_DESC) {
+//       ...TradesFragment
+//     }
+//   }
+//   ${TRADES_FRAGMENT}
+// `
+//
+// export {
+//   TRADES_LIST,
+// }
