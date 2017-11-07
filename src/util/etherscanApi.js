032b003545dfa32be7755b40ed68d4cb84a3c290
akalajzi <akalajzi@gmail.com>
2017-11-07 13:23:10 +0100

    Add etherscanApi

---
+++ src/util/etherscanApi.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+import _ from 'lodash'
+
+export const ETHERSCAN_API = 'http://api.etherscan.io/api'
+export const ETHERSCAN_API_KEY = 'GSE8Z3UJYV6N9XAKFC1F9J8EJ7FN6QA9H2'
+
+export function createTransactionListPayload(address, startblock = 0, endblock = 99999999) {
+  return {
+    module: 'account',
+    action: 'txlist',
+    address,
+    startblock,
+    endblock,
+    sort: 'desc',
+    apikey: ETHERSCAN_API_KEY,
+  }
+}
+
+export function fetchEtherscan(address, payload) {
+  return axios.get(ETHERSCAN_API, payload)
+}
