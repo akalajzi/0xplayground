060808be34552f901bdd0c7dd6af8d57d2d3f826
akalajzi <akalajzi@gmail.com>
2017-11-07 12:50:34 +0100

    Clean network reducer

--- src/reducers/network.js
+++ src/reducers/network.js
@@ -3,12 +3,6 @@ import _ from 'lodash'
 export const initialState = {
   id: null,
   blockHeight: null,
-  lastFetchedBlock: null,
-  contractAddress: null,
-  zrxContractAddress: null,
-  tokens: null,
-  logs: null,
-  latestTrades: [],
 }
 
 export default function reducer(state, action) {
@@ -19,41 +13,8 @@ export default function reducer(state, action) {
       return initialState
     case 'network/SET_BLOCK_HEIGHT':
       return { ...state, blockHeight: action.block }
-    case 'network/SET_CONTRACT_ADDRESS':
-      return { ...state, contractAddress: action.address }
-    case 'network/ADD_LOG':
-      newLogs[_.trimStart(action.data.transactionHash, '0x')] = action.data
-      return {
-        ...state,
-        logs: newLogs,
-      }
-    case 'network/SET_LATEST_TRADES':
-      // newTrades = _.concat(state.latestTrades, action.trades)
-      newTrades = _.unionBy(state.latestTrades, action.trades, 'transactionHash')
-      return {
-        ...state,
-        latestTrades: newTrades,
-      }
-    case 'network/SET_LOGS':
-      // TODO: No good, fixme
-      return {
-        ...state,
-        logs: action.logs,
-        lastFetchedBlock: action.fromBlock,
-      }
     case 'network/SET_NETWORK':
       return { ...state, id: action.id }
-    case 'network/SET_TIMESTAMP_TRADE':
-      newLogs = {...state.logs}
-      newLogs[action.blockNumber] = action.trade
-      return {
-        ...state,
-        logs: newLogs,
-      }
-    case 'network/SET_TOKENS':
-      return { ...state, tokens: action.tokens }
-    case 'network/SET_ZRX_CONTRACT_ADDRESS':
-      return { ...state, zrxContractAddress: action.address }
     default:
       return state
   }
@@ -63,45 +24,6 @@ export function setBlockHeight(block) {
   return { type: 'network/SET_BLOCK_HEIGHT', block }
 }
 
-export function setContractAddress(address) {
-  return { type: 'network/SET_CONTRACT_ADDRESS', address}
-}
-
-export function addLog(data) {
-  return null
-  return {
-    type: 'network/ADD_LOG', data
-  }
-}
-
-export function setLogs(data) {
-  return {
-    type: 'network/SET_LOGS',
-    logs: data.logs,
-    fromBlock: data.fromBlock,
-  }
-}
-
-export function setLatestTrades(trades) {
-  return { type: 'network/SET_LATEST_TRADES', trades }
-}
-
 export function setNetwork(id) {
   return { type: 'network/SET_NETWORK', id }
-}
-
-export function setTimestampOnTrade(data) {
-  return {
-    type: 'network/SET_TIMESTAMP_TRADE',
-    blockNumber: data.blockNumber,
-    trade: data.timestampedTrade,
-  }
-}
-
-export function setTokens(tokens) {
-  return { type: 'network/SET_TOKENS', tokens }
-}
-
-export function setZrxContractAddress(address) {
-  return {type: 'network/SET_ZRX_CONTRACT_ADDRESS', address}
 }
